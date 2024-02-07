51dab61eefcccbb148fd117d65e2301330dd2b6f
Sanecek11 <alexandr.prosto.pro@mail.ru>
2024-02-07 19:34:28 +0300

    finish

--- landing/urls.py
+++ landing/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index_view, TemplView
+from .views import index_view, TemplViewForm
 urlpatterns = [
-    path('landing/', TemplView.as_view(), name='landing'),
+    path('', TemplViewForm.as_view(), name='landing'),
+    # path('fv/', FV.as_view(), name='landing'),
     path('', index_view, name='index'),
     # добавьте здесь маршрут для вашего обработчика отображения страницы приложения landing
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,77 +2,58 @@ from django.shortcuts import render, redirect
 from .forms import TemplateForm
 from django.views import View
 from django.http import HttpRequest, JsonResponse
-
-
-# Create your views here.
-class TemplView(View):
-    def get(self, request):
-        return render(request, 'landing/index.html')
-
-    def post(self, request):
-        received_data = request.POST
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        user_agent = request.META.get('HTTP_USER_AGENT')
-
-        form = TemplateForm(received_data)
-        name = email = message = ''
-        if form.is_valid():
-            name = form.cleaned_data.get("name")
-            email = form.cleaned_data.get("email")
-            message = form.cleaned_data.get("message")
-
-        response_data = {
-            'form_data': received_data,
-            'name': name,
-            'email': email,
-            'message': message,
-            'ip_address': ip,
-            'user_agent': user_agent
-        }
-
-        return JsonResponse(response_data)
-
-
- def index_view(request):
-     if request.method == "GET":
-         return render(request, 'landing/index.html')
+from django.views.generic import FormView
 
 
 # class TemplView(View):
 #     def get(self, request):
-#
-#         if request.method == "GET":
-#             return render(request, 'landing/index.html')
+#         return render(request, 'landing/index.html')
 #
 #     def post(self, request):
-#         if request.method == "POST":
-#             received_data = request.POST  # Приняли данные в словарь
-#             x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-#             if x_forwarded_for:
-#                 ip = x_forwarded_for.split(',')[0]  # Getting the IP
-#             else:
-#                 ip = request.META.get('REMOTE_ADDR')  # Getting the IP
-#             user_agent = request.META.get('HTTP_USER_AGENT')
-#             form = TemplateForm(received_data)  # Передали данные в форму
-#             if form.is_valid():  # Проверили, что данные все валидные
-#                 name = form.cleaned_data.get("name")  # Получили очищенные данные
-#                 email = form.cleaned_data.get("email")
-#                 message = form.cleaned_data.get("message")
+#         received_data = request.POST
+#         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+#         if x_forwarded_for:
+#             ip = x_forwarded_for.split(',')[0]
+#         else:
+#             ip = request.META.get('REMOTE_ADDR')
+#         user_agent = request.META.get('HTTP_USER_AGENT')
+#
+#         form = TemplateForm(received_data)
+#         if form.is_valid():
+#             name = form.cleaned_data.get("name")
+#             email = form.cleaned_data.get("email")
+#             message = form.cleaned_data.get("message")
+#
 #             response_data = {
-#                 'form_data': request.POST,
 #                 'name': name,
 #                 'email': email,
 #                 'message': message,
 #                 'ip_address': ip,
 #                 'user_agent': user_agent
-#                 }
-#
-#             return JsonResponse(response_data)
-#         else:
-#                 # return JsonResponse({'статус': 'Данные успешно отправлены!'})
-#                 # return JsonResponse(form.cleaned_data)
-#             return render(request, 'landing/index.html', context={"form": form})
+#             }
+#             return JsonResponse(response_data, json_dumps_params={"indent": 4})
+
+
+class TemplViewForm(FormView):
+    template_name = 'landing/index.html'
+    form_class = TemplateForm
+
+    def form_valid(self, form):
+        x_forwarded_for = self.request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = self.request.META.get('REMOTE_ADDR')
+        user_agent = self.request.META.get('HTTP_USER_AGENT')
+
+        data = form.cleaned_data
+        data["ip"] = ip
+        data["user_agent"] = user_agent
+
+        return JsonResponse(data, json_dumps_params={"indent": 4})
+
+
+
+def index_view(request):
+    if request.method == "GET":
+        return render(request, 'landing/index.html')
